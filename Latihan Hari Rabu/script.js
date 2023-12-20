a6c3c762e19490a18d253b444e1d18f5e725d773
NrthSS <nurrizkyr90@gmail.com>
2023-12-20 11:14:18 +0700

    first commit

---
+++ Latihan Hari Rabu/script.js
@@ -0,0 +1,119 @@
+// 3 buang angka dan menentukan bilangan terbesar 
+// NO 1
+// NO 1
+let A = 90;
+let B = 100;
+let C = 3;
+
+if(A>B && A>C) {
+    console.log("A = "+A+" ")
+}
+else if (B>A && B>C) {
+    console.log("B = "+B+" ")
+}
+else {
+    console.log("C = "+C+" ")
+}
+
+
+
+
+
+
+// LAmpu
+// NO 3
+// NO 3
+let lampu = true;
+
+if(lampu == true) {
+    result = "on"
+} 
+else if(lampu == false) {
+    result = "off"
+}
+
+console.log(result)
+
+
+
+
+
+
+// Masukan 3 harga barang.
+// NO 4
+// NO 4
+let harga1 = 90000;
+let harga2 = 8000;
+let harga3 = 200000;
+let diskon = 10/100;
+
+let Total = harga1 + harga2 + harga3;
+
+if(Total > 10000) {
+   TotalHarga = Total-(diskon * Total);
+}
+else {
+   TotalHarga = Total;
+}
+
+console.log(TotalHarga)
+
+
+
+
+
+
+// NO 5
+// NO 5
+
+let lembur = 7;
+
+if(lembur < 6) {
+   console.log('Gaji lembur Rp. 100.000')
+}
+else if(lembur == 6) {
+   console.log('Gaji lembur Rp. 200.000')
+}
+else {
+   console.log('gaji lembur Rp. 300.000')
+}
+
+
+
+
+
+
+// NO 6
+// NO 6
+
+let tinggiAir = 650.0;
+
+if(tinggiAir <= 500.0) {
+    console.log('Status Aman')
+}
+else if(tinggiAir <= 600.1) {
+    console.log('Status WASPADA')
+}
+else if(tinggiAir <= 650.0) {
+    console.log('Siaga 2')
+}
+else if(tinggiAir > 650){
+    console.log('Siaga 1')
+}
+
+
+
+
+
+
+
+// TUGAS DARI WhatsApp
+// +
+// +
+
+let JumlahTiketVIP = 50;
+let JumlahTiketEKSEKUTIF = 50;
+let JumlahTiketEKENOMI = 50;
+
+
+
